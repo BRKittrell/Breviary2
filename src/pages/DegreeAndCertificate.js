041b0c70338810b7a2f4fb84db158411fb89906f
Brandon Kittrell <brandonkittrell@Brandons-MBP.lan1>
2023-09-23 12:51:31 -0400

    edited education

--- src/pages/DegreeAndCertificate.js
+++ src/pages/DegreeAndCertificate.js
@@ -9,10 +9,12 @@ return(
     <iframe className="professional" src="https://drive.google.com/file/d/1_KdetbTDHHrNNPcjefjGKY67iXkMO3y2/preview" width="640" height="480" allow="autoplay"></iframe>
     <h1>Software Engineering Certificate</h1>
     <iframe className="professional" src="https://drive.google.com/file/d/1KCwNg4ZJ233UMIMrFNcYIQE7T_6Mf_XK/preview" width="640" height="480" allow="autoplay"></iframe>
-    <h1>Joint Services Transcript</h1>
-    <iframe className="professional" src="https://drive.google.com/file/d/1YlLnoWFjbymqhzgHf_Xf_V6zW2ZqlpQ5/preview" width="640" height="480" allow="autoplay"></iframe>
     <h1>Software Engineernig Curriculum</h1>
     <iframe className="professional" src="https://drive.google.com/file/d/1Rxgt8m8NoLyitq6mPk9p9x2_bGQsoV9C/preview" width="640" height="480" allow="autoplay"></iframe>
+    <h1>Penn State Degree Progress</h1>
+    <iframe src="https://drive.google.com/file/d/1xco24BB_6JHS3QYPkijOeTZhnRDaDLb0/preview" width="640" height="480" allow="autoplay"></iframe>
+    <h1>Joint Services Transcript</h1>
+    <iframe className="professional" src="https://drive.google.com/file/d/1YlLnoWFjbymqhzgHf_Xf_V6zW2ZqlpQ5/preview" width="640" height="480" allow="autoplay"></iframe>
     </>
 )
 }
