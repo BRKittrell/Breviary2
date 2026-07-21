920d90408fb790e30fab6df099940826f2a7d2e9
Brandon Kittrell <brandonkittrell@Brandons-MacBook-Pro.local>
2026-07-21 15:48:17 -0500

    modified credentials titles

--- src/pages/DegreeAndCertificate.js
+++ src/pages/DegreeAndCertificate.js
@@ -3,13 +3,13 @@ import React from "react";
 const DegreeAndCertificates = () =>{
 return(
     <>
-    <h1>Bachelor of Science in Business</h1>
+    <h1>B.S. in Business</h1>
     <iframe className="professional"src="https://drive.google.com/file/d/162PjQWlFNqgtYLi9ujXfhPTiDemizoXo/preview" width="640" height="480" allow="autoplay"></iframe>
-    <h1>Associate of Arts Business Management</h1>
+    <h1>A.A. in Business Management</h1>
     <iframe className="professional"src="https://drive.google.com/file/d/13ocdgCLAn_KxJp5CtiIXhC4GNUMAeBMX/preview" width="640" height="480" allow="autoplay"></iframe>
-    <h1>Professional Scrum Master Certificate</h1>
+    <h1>Professional Scrum Master I</h1>
     <iframe className="professional" src="https://drive.google.com/file/d/1_KdetbTDHHrNNPcjefjGKY67iXkMO3y2/preview" width="640" height="480" allow="autoplay"></iframe>
-    <h1>Software Engineering Certificate</h1>
+    <h1>SWE Certificate</h1>
     <iframe className="professional" src="https://drive.google.com/file/d/1KCwNg4ZJ233UMIMrFNcYIQE7T_6Mf_XK/preview" width="640" height="480" allow="autoplay"></iframe>
     <h1>Joint Services Transcript</h1>
     <iframe className="professional" src="https://drive.google.com/file/d/1TVRi8ytTKx7zj8WTJ2rISbdrGHmJyVro/preview" width="640" height="480" allow="autoplay"></iframe> 
